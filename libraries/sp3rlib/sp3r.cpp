69ab25a35e8dd06c785eb2106375e52a2e373229
Mark A. Caprio <mcaprio@nd(dot)edu>
2016-03-09 19:13:56 -0500

    More work on sp3r.

--- libraries/sp3rlib/sp3r.cpp
+++ libraries/sp3rlib/sp3r.cpp
@@ -20,17 +20,58 @@ namespace sp3r
   // space and subspace indexing
   ////////////////////////////////////////////////////////////////
 
-  U3Subspace::U3Subspace(const u3::U3& w, const SpanakopitaType& states)
+  U3Subspace::U3Subspace(const u3::U3& w, const SpanakopitaRangeType& state_range)
   {
 
     // set subspace labels
     labels_ = w;
 
     // set up indexing of states
-    SpanakopitaRangeType state_range = states.equal_range(w);
     for (SpanakopitaType::iterator it = state_range.first; it != state_range.second; ++it)
-      PushStateLabels(it->second); 
+      {
+	// need to expand rho_max
+	// need comparison function for MultiplicityTagged
+	//PushStateLabels(it->second); 
+      }
 
+  }
+
+
+  Sp3RSpace::Sp3RSpace(const u3::U3& sigma, int Nn_max)
+  {
+
+    // set space labels
+    sigma_ = sigma;
+    Nn_max_ = Nn_max;
+
+    // make spanakopita
+    
+    SpanakopitaType states;
+
+    // scan through spanakopita
+    //   by w key group (subspace)
+
+    SpanakopitaType::iterator it = states.begin();
+    while (it != states.end())
+      {
+	// retrieve w key of this group of states
+	u3::U3 w = it->first;
+
+	// find range of this group of states
+	SpanakopitaRangeType state_range = states.equal_range(w);
+
+	// set up subspace
+	//
+	// Note: Creation followed by push-back means U3Subspace is
+	// *copied* into vector.  If this overhead is troublesome, can
+	// replace U3Subspace constructor with lightweight constructor
+	// which just saves w.  Push the (empty) subspace into the
+	// Sp3RSpace.  Then use an Init method to actually populate
+	// the space once it is in the vector.
+
+	
+
+      }
   }
 
 }  // namespace
