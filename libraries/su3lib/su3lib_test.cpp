b3e4a1e7b1d4d521db0b965c6b0e2f5db79ff46e
Mark A. Caprio <mcaprio@nd(dot)edu>
2016-03-10 19:32:48 -0500

    Sending it on back...

--- libraries/su3lib/su3lib_test.cpp
+++ libraries/su3lib/su3lib_test.cpp
@@ -1,38 +1,81 @@
+/****************************************************************
+  su3lib_test.cpp
+
+  Simple testbed program for C++ linkage from SU3LIB.
+ 
+  Compilation and linkage:
+    g++ -c su3lib_test.cpp
+    g++ -o su3lib_test *.o -lgfortran
+
+
+  Anna E. McCoy and Mark A. Caprio
+  University of Notre Dame
+
+  3/9/16 (aem,mac): Created.
+
+****************************************************************/
+
+
+
 #include <cstdlib>
+#include <iostream>
 
 
 
 const size_t MAX_K = 9;
 
 extern "C" 
-		{ 
-			extern void wu3r3w_(const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, double[MAX_K][MAX_K][MAX_K][MAX_K]);
-			extern void wru3optimized_(const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, double[], const int&);
-			extern void wzu3optimized_(const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, double[], const int&);
-			//extern void xewu3(const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, double[], int[], int[], int[], const int&, const int&, const int&);
-			//extern void xwu3(const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, double[], const int&, const int&, double[], int[], int[], int[], int[], const int&, const int&, int[], const int&, const int&, const int&, const int&);
-			extern void wu39lm_(const int&, const int& , const int&, const int&, const int& , const int& , const int& , const int&, const int&, const int&, const int&, const int&, const int& , const int& , const int& , const int&, const int&, const int&, double[], const int&);
-			extern void blocks_(void);
-		}
+{ 
+  extern void wu3r3w_(const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, double[MAX_K][MAX_K][MAX_K][MAX_K]);
+  extern void wru3optimized_(const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, double[], const int&);
+  extern void wzu3optimized_(const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, double[], const int&);
+  //extern void xewu3(const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, double[], int[], int[], int[], const int&, const int&, const int&);
+  //extern void xwu3(const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, const int&, double[], const int&, const int&, double[], int[], int[], int[], int[], const int&, const int&, int[], const int&, const int&, const int&, const int&);
+  extern void wu39lm_(const int&, const int& , const int&, const int&, const int& , const int& , const int& , const int&, const int&, const int&, const int&, const int&, const int& , const int& , const int& , const int&, const int&, const int&, double[], const int&);
+  extern void blocks_(void);
+}
+
+void W_test()
+{
+    
+  int x1lambda = 2;
+  int x1mu = 0;
+  int x2lambda = 1;
+  int x2mu = 0;
+  int x3lambda = 3;
+  int x3mu = 0;
+
+  int k1 = 1;
+  int L1 = 0;
+  int k2 = 1;
+  int L2 = 1;
+  int k3 = 1;
+  int L3 = 1;
+
+  int k1max = 1; // not used
+  int k2max = 1;
+  int k3max = 1;
+
+  int r0 = 1;
+
+  double w_array[MAX_K][MAX_K][MAX_K][MAX_K];
+
+  wu3r3w_(
+	  x1lambda, x1mu, x2lambda, x2mu, x3lambda, x3mu, 
+	  L1, L2, L3, 
+	  r0, 1, 1, 1, 
+	  w_array
+	  );
+  double c = w_array[r0-1][k1-1][k2-1][k3-1];
+
+  std::cout << c << std::endl;
+
+
+}
 
 int main(int argc, char **argv)
 {
-	blocks_();
+  blocks_();
+  W_test();
 }
 
-double W(const u3::SU3& x1, int k1, int L1, const u3::SU3& x2, int k2, int L2, const u3::SU3& x3, int k3, int L3, int r0)
-  {
-    
-
-    //int r_max=u3::OuterMultiplicity(x1,x2,x3);
-    //int k1max=BranchingMultiplicity(x1, L1);
-    //int k2max=BranchingMultiplicity(x2, L2);
-    //int k3max=BranchingMultiplicity(x3, L3);
-    //int dummy=1;
-    // Dimension of array that is returned contained coefficients is 9. 
-    double w_array[su3lib::MAX_K][su3lib::MAX_K][su3lib::MAX_K][su3lib::MAX_K];
-    su3lib::wu3r3w_(x1.lambda, x1.mu, x2.lambda, x2.mu, x3.lambda, x3.mu, L1 , L2, L3, r0,1,1,1, w_array);
-    return w_array[r0-1][k1-1][k2-1][k3-1];
-}
-//g++ -c su3lib_test.cpp
-//g++ -o su3lib_test blocks.o su3lib_test.o -lgfortran
