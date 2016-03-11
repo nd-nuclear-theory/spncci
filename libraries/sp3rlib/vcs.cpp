aa7b31097357f21aa429831f43c9bd6a607904d6
Anna McCoy <annamccoy@macbook-pro-3.dhcp.nd.edu>
2016-03-11 00:06:54 -0500

    Update vcs.ccp.

--- libraries/sp3rlib/vcs.cpp
+++ libraries/sp3rlib/vcs.cpp
@@ -11,9 +11,12 @@
   3/9/16 (aem): Created based on prototype vcs.py.
 
 ****************************************************************/
+
 #include <Cmath>  
-#include "u3.h"
-#include <Eigen.h>
+
+#include "sp3rlib/u3.h"
+#include "sp3rlib/vcs.h"
+
 
 
 namespace vcs{
@@ -42,9 +45,61 @@ namespace vcs{
 
   	}
 
-  double SMatrix(u3::U3& s, u3::U3& s, MultiplicityTagged<u3::U3>& nr1, MultiplicityTagged<u3::U3>& nr2)
+  double SMatrix(u3::U3& s, u3::U3& wp, MultiplicityTagged<u3::U3>& nr1p, MultiplicityTagged<u3::U3>& nr2p)
   {
-    double smatrix=0;
+    double smatrix=0.0;
+    u3::U3 n1p=nr1p.irrep;
+    u3::U3 n2p=nr2p.irrep;
+    std::vector<u3::U3> n1set;
+    std::vector<u3::U3> n2set;
+    
+    if (n1p.f1-2>=n1p.f2)
+      n1set.push_back(u3::U3(n1p.f1-2,n1p.f2,n1p.f3));
+    if (n1p.f2-2>=n1p.f3)
+      n1set.push_back(u3::U3(n1p.f1,n1p.f2-2,n1p.f3));
+    if (n1p.f3-2>=0)
+      n1set.push_back(u3::U3(n1p.f1,n1p.f2,n1p.f3-2));
+    
+    if (n2p.f1-2>=n2p.f2)
+      n2set.push_back(u3::U3(n2p.f1-2,n2p.f2,n2p.f3));
+    if (n2p.f2-2>=n2p.f3)
+      n2set.push_back(u3::U3(n2p.f1,n2p.f2-2,n2p.f3));
+    if (n2p.f3-2>=0)
+      n2set.push_back(u3::U3(n2p.f1,n2p.f2,n2p.f3-2));
+
+    MultiplicityTagged<u3::U3>::vector wset=KroneckerProduct(wp, u3::U3(0,0,-2));
+    for (int i=0; i<wset.size(); i++)
+      {
+        u3::U3 w=wset[i].irrep;
+        for(int j=0; j<n1set.size(); j++)
+          {
+            u3::U3 n1=n1set[j];
+            int r1_max=OuterMultiplicity(s.SU3(),n1.SU3(),w.SU3());
+            for(r1=1; r1<=r1_max; r1++)
+              {
+                MultiplicityTagged<u3::U3 nr1(n1,r1);
+                for(k=0; k<n2set.size(); k++)
+                  {
+                    u3::U3 n2=n2set[k];
+                    int r2_max=OuterMultiplicity(s.SU3(),n2.SU3(),w.SU3());
+                    for (r2=1; r2<=r2_max; r2++)
+                      {
+                        MultiplicityTagged<u3::U3 nr2 (n2,r12);
+                        smatrix+=(
+                          (Omega(n1p, wp)-Omega(n1,w))
+                          *
+                          *BosonCreationRME(n1p,n1)
+                          )
+                      }
+
+                  }
+              }
+          }
+
+      }
+
+
+
 
 
     return smatrix;
