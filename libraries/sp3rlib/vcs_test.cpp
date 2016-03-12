49c73e97ec9114f1d6ceeac4bdc26a63df8e6c1e
Anna McCoy <annamccoy@macbook-pro-3.dhcp.nd.edu>
2016-03-11 23:36:22 -0500

    Added Kmatrix to vcs

--- libraries/sp3rlib/vcs_test.cpp
+++ libraries/sp3rlib/vcs_test.cpp
@@ -17,6 +17,7 @@
 
 #include "sp3rlib/u3.h"
 #include "sp3rlib/vcs.h"
+#include "sp3rlib/sp3r.h"
 
 
 int main(int argc, char **argv)
@@ -29,37 +30,39 @@ int main(int argc, char **argv)
     u3::U3 nn(4,0,0);
     //  4.375.
 
-	// std::cout<< vcs::Omega(nn, w) << std::endl;
+	std::cout<< vcs::Omega(nn, w) << std::endl;
 
-	// ////////////////////////////////////////////////////////
-	// // BosonCreationRME test
-	// ////////////////////////////////////////////////////////
-	// // Checked against formula  
-	// // np.f1=n.f1+2
-	// u3::U3 np(6,4,2);
-	// u3::U3 n(4,4,2);
-	// std::cout << vcs::BosonCreationRME(np,n) << std::endl;
-	// // np.f2=n.f2+2
-	// np=u3::U3(6,4,2);
-	// n=u3::U3(6,2,2);
-	// std::cout << vcs::BosonCreationRME(np,n) << std::endl;
-	// // np.f3=n.f3+2
-	// np=u3::U3(6,4,2);
-	// n=u3::U3(6,4,0);
-	// std::cout << vcs::BosonCreationRME(np,n) << std::endl;
+	////////////////////////////////////////////////////////
+	// BosonCreationRME test
+	////////////////////////////////////////////////////////
+	// Checked against formula  
+	// np.f1=n.f1+2
+	u3::U3 np(6,4,2);
+	u3::U3 n(4,4,2);
+	std::cout << vcs::BosonCreationRME(np,n) << std::endl;
+	// np.f2=n.f2+2
+	np=u3::U3(6,4,2);
+	n=u3::U3(6,2,2);
+	std::cout << vcs::BosonCreationRME(np,n) << std::endl;
+	// np.f3=n.f3+2
+	np=u3::U3(6,4,2);
+	n=u3::U3(6,4,0);
+	std::cout << vcs::BosonCreationRME(np,n) << std::endl;
 
 	////////////////////////////////////////////////////////
 	//  Smatrix 
 	////////////////////////////////////////////////////////
 	u3::U3 s(20,13,10);
-	w=u3::U3(22,15,10);
-	MultiplicityTagged<u3::U3> nr1(u3::U3(2,2,0),1);
-	MultiplicityTagged<u3::U3> nr2(u3::U3(4,0,0),1);
-	std::cout<<"hi  "<<vcs::SMatrix(s,w,nr1,nr1)<<std::endl;
-	std::cout<<vcs::SMatrix(s,w,nr2,nr1)<<std::endl;
-	std::cout<<vcs::SMatrix(s,w,nr1,nr2)<<std::endl;
-	std::cout<<vcs::SMatrix(s,w,nr2,nr2)<<std::endl;
+	sp3r::Sp3RSpace irrep(s,4);
+	std::map<u3::U3,Eigen::MatrixXd> K_matrix_map;
 
+  	vcs::GenerateKMatrices(irrep,K_matrix_map);
+
+  	//std::map<u3::U3,Eigen::MatrixXd>::const_interator it=K_matrix_map.begin();
+  	//while (int it!=K_matrix_map.end())
+  	//	{
+  	//		std::cout<< it->first.Str() <<it->second<<std::endl;
+  	//	}
 
       // s=U3State([20,13,10])
       // w=U3State([22,15,10])
