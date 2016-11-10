bd5e2ad4f60a986bff10a947d9f8bca81cf89d68
A E McCoy <amccoy@nd.edu>
2016-11-10 15:30:31 -0500

    Work in progress.

--- libraries/lsu3shell/lsu3shell_rme_test.cpp
+++ libraries/lsu3shell/lsu3shell_rme_test.cpp
@@ -11,7 +11,8 @@
 #include <ostream>  
 #include "cppformat/format.h"
 #include "sp3rlib/u3coef.h"
-
+#include "lgi/lgi_solver.h"
+#include <eigen3/Eigen/LU>
 
 int main(int argc, char **argv)
 {
@@ -42,28 +43,45 @@ int main(int argc, char **argv)
   for(int i=0; i<matrices.size(); ++i)
   {
     // if(fabs(matrices[i].sum())>10e-13)
-      std::cout<<matrices[i]<<std::endl;
-      std::cout<<"  "<<std::endl;
+      // std::cout<<matrices[i]<<std::endl;
+      std::cout<<"eigenvalues"<<std::endl;
+
       Eigen::VectorXcd eigs=matrices[i].eigenvalues();
-      std::cout<<eigs<<std::endl<<std::endl;
+      for(int j=0; j<eigs.size(); ++j)  
+        std::cout<<eigs(j).real()<<std::endl;
+  }
+  basis::MatrixVector ncm_matrices;
+  lgi::GenerateNcmMatrixVector(Nsigma_0,op_filename,basis_table,space,ncm_matrices);
+  for(int i=0; i<ncm_matrices.size(); ++i)
+  {
+    // if(fabs(matrices[i].sum())>10e-13)
+      // std::cout<<matrices[i]<<std::endl;
+      std::cout<<"eigenvalues"<<std::endl;
+
+      Eigen::VectorXcd eigs=ncm_matrices[i].eigenvalues();
+      for(int j=0; j<eigs.size(); ++j)  
+        std::cout<<eigs(j).real()<<std::endl;
+
+      Eigen::MatrixXd null=ncm_matrices[i].fullPivLu().kernel();
+      std::cout<<"null space"<<std::endl<<null<<std::endl<<std::endl;
   }
 
   // test matrix comparison
   //
-  // This is just a self-comparison, with one entry skewed...
-  basis::MatrixVector matrices_mod = matrices;
-  matrices_mod[0](0,0) += 3.14159;
-  double epsilon = 1e-8;
-  CompareLSU3ShellRMEs(
-      std::cout,
-      basis_provenance,
-      space, 
-      sectors,
-      matrices,
-      matrices_mod,
-      epsilon,
-      true  // verbose
-    );
+  // // This is just a self-comparison, with one entry skewed...
+  // basis::MatrixVector matrices_mod = matrices;
+  // matrices_mod[0](0,0) += 3.14159;
+  // double epsilon = 1e-8;
+  // CompareLSU3ShellRMEs(
+  //     std::cout,
+  //     basis_provenance,
+  //     space, 
+  //     sectors,
+  //     matrices,
+  //     matrices_mod,
+  //     epsilon,
+  //     true  // verbose
+  //   );
 
 
 }
