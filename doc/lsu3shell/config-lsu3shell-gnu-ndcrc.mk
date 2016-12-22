6472ae6d9624a6b37deca23f51b2ec1aa2c48907
Mark A. Caprio <mcaprio-nd-edu>
2016-12-22 14:30:38 -0500

    config: Reconcile config with shell project and update lsu3shell config

--- doc/lsu3shell/config-lsu3shell-gnu-ndcrc.mk
+++
@@ -1,3 +0,0 @@
-include config-lsu3shell-gnu.mk
-
-LDLIBS += /afs/crc.nd.edu/x86_64_linux/scilib/acml/current/gfortran/gfortran64/lib/libacml.a
---
+++ doc/lsu3shell/config/module-load-nersc.csh
@@ -0,0 +1,1 @@
+../../../config/module-load-nersc.csh
