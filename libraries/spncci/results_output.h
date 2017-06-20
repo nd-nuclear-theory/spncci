5d4d3ed2aafb5252ed3250471e70c11c4b153b00
Mark A. Caprio <mcaprio-nd-edu>
2017-06-20 13:41:09 -0400

    spncci: Implement basic results output

--- libraries/spncci/results_output.h
+++ libraries/spncci/results_output.h
@@ -15,15 +15,76 @@
 #include <iostream>
 #include <string>
 
+#include "cppformat/format.h"
 #include "spncci/branching.h"
 #include "spncci/parameters.h"
 
 namespace spncci
 {
 
-  void WriteResultsHeader(std::ostream& out_stream, const spncci::RunParameters& run_parameters);
+  ////////////////////////////////////////////////////////////////
+  // version stamp
+  ////////////////////////////////////////////////////////////////
+  const int g_results_version = 201706200; // "yyyymmddv" (v=version w/in day)
 
-  void WriteResultsBasis(
+  ////////////////////////////////////////////////////////////////
+  // output utilities
+  ////////////////////////////////////////////////////////////////
+
+  void StartNewSection(std::ostream& out_stream, const std::string& title);
+  // Start new file section.
+  //
+  // Arguments:
+  //   out_stream (input): output stream
+  //   title (input): section title
+
+  template <typename tValue>
+    void WriteKeyValue(std::ostream& out_stream, const std::string& keyword, const std::string& format, tValue value)
+  // Write key-value pair.
+  //
+  // Arguments:
+  //   out_stream (input): output stream
+  //   title (input): keyword
+  //   format (input): format code for value (no braces!)
+  //   value (input): value to write
+  {
+    std::string full_format = fmt::format("{{{}}}",format);  // encapsulate format code in braces
+    out_stream << fmt::format("{} = {}",keyword,fmt::format(full_format,value)) << std::endl;
+  }
+
+  template <typename tValueIterable>
+    void WriteKeyValueList(
+        std::ostream& out_stream, const std::string& keyword, const std::string& format,
+        tValueIterable data
+      )
+    // Write key-value-list pair.
+    //
+    // The data may be any iterable container of values, such as std::vector or std::array.
+    //
+    // Arguments:
+    //   out_stream (input): output stream
+    //   title (input): keyword
+    //   format (input): format code for value (no braces!)
+    //   date (input): values to write
+    {
+    std::string full_format = fmt::format("{{{}}}",format);  // encapsulate format code in braces
+    out_stream << fmt::format("{} =",keyword);
+    for (const auto& value : data)
+      out_stream << " " << fmt::format(full_format,value);
+    out_stream << std::endl;
+      
+  }
+
+
+  ////////////////////////////////////////////////////////////////
+  // output code
+  ////////////////////////////////////////////////////////////////
+
+  void WriteCodeInformation(std::ostream& out_stream, const spncci::RunParameters& run_parameters);
+
+  void WriteRunParameters(std::ostream& out_stream, const spncci::RunParameters& run_parameters);
+
+  void WriteBasisStatistics(
       std::ostream& out_stream,
       const spncci::SpNCCISpace& spncci_space,
       const spncci::BabySpNCCISpace& baby_spncci_space,
@@ -31,10 +92,26 @@ namespace spncci
       const spncci::SpaceSpLS spls_space
     );
 
-  void WriteResultsEigenvalues(
-      std::ostream& out_stream
-      // TODO
+  void WriteCalculationParameters(
+      std::ostream& out_stream,
+      double hw
     );
+
+  void WriteEigenvalues(
+      std::ostream& out_stream,
+      const std::map<HalfInt,Eigen::VectorXd>& eigenvalues
+    );
+  // Write table of eigenvalues.
+  //
+  // CAVEAT: parity quantum number in output is hard-coded to natural
+  // parity
+  //
+  // TODO: revise to take SpJSpace and std::vector<Eigen::VectorXd>,
+  // and then take J and gex from SpJSubspace labels
+  //
+  // Arguments:
+  //   out_stream (input): output stream
+  //   eigenvalues (input): map of J -> eigenvalues
 
 }  // namespace
 
