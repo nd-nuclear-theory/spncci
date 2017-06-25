0185ffcc6b08799ea7dcd130d32d6d8357a13b22
Mark A. Caprio <mcaprio-nd-edu>
2017-06-25 15:35:06 -0400

    su3calc: Add SO(3) branching calculation

--- programs/su3calc/su3calc.cpp
+++ programs/su3calc/su3calc.cpp
@@ -1,17 +1,25 @@
 /****************************************************************
   su3calc.cpp
 
-  Calculate and output product of two SU(3) irreps.
+  Calculate and output SU(3) quantities, e.g., product of two SU(3)
+  irreps.
 
   Examples:
 
-    >>> KroneckerProduct 1 1 1 1
+    >>> product 1 1 1 1
     (1,1) x (1,1):
       ((0,0),1)
       ((0,3),1)
       ((1,1),2)
       ((2,2),1)
       ((3,0),1)
+
+    >>> branching 2 2
+    (2,2):
+      (0,1)
+      (2,2)
+      (3,1)
+      (4,1)
 
     >>> W 2 0 1 0 0 0 1 0 2 0 1 0 1
     (2,0) 1 0 x (0,0) 1 0 -> (0,0) 1 0 1
@@ -28,6 +36,7 @@
   University of Notre Dame
 
   10/17/16 (mac): Create, extending su3_coupler.
+  6/25/17 (mac): Update keywords.  Add SO(3) branching.
 
 ****************************************************************/
 
@@ -48,7 +57,8 @@
 void DoHelp()
 {
   std::cout << "Syntax:" << std::endl;
-  std::cout << "  KroneckerProduct lambda1 mu1 lambda2 mu2" << std::endl;
+  std::cout << "  product lambda1 mu1 lambda2 mu2" << std::endl;
+  std::cout << "  branching lambda mu" << std::endl;
   std::cout << "  W lambda1 mu1 kappa1 L1 lambda2 mu2 kappa2 L2 lambda3 mu3 kappa3 L3 rho0" << std::endl;
   std::cout << "  U lambda1 mu1 lambda2 mu2 lambda mu lambda3 mu3 lambda12 mu12 rho12 rho12_3 lambda23 mu23 rho23 rho1_23" << std::endl;
   std::cout << std::endl;
@@ -63,9 +73,21 @@ void DoKroneckerProduct(int lambda1, int mu1, int lambda2, int mu2)
   std::cout << fmt::format("{} x {}:",x1.Str(), x2.Str()) << std::endl;
 
   // calculate
-  MultiplicityTagged<u3::SU3>::vector product=KroneckerProduct(x1,x2);
+  MultiplicityTagged<u3::SU3>::vector product = u3::KroneckerProduct(x1,x2);
   for(int i=0; i<product.size(); ++i)  
     std::cout << "  " << product[i].Str() << std::endl;
+}
+
+void DoBranchingSO3(int lambda, int mu)
+{
+  // label
+  u3::SU3 x(lambda,mu); 
+  std::cout << fmt::format("{}:",x.Str()) << std::endl;
+
+  // calculate
+  MultiplicityTagged<int>::vector branching = u3::BranchingSO3(x);
+  for(int i=0; i<branching.size(); ++i)  
+    std::cout << "  " << branching[i].Str() << std::endl;
 }
 
 void DoW(int lambda1, int mu1, int kappa1, int L1, int lambda2, int mu2, int kappa2, int L2, int lambda3, int mu3, int kappa3, int L3, int rho0)
@@ -159,7 +181,16 @@ int main(int argc, char **argv)
         {
           DoHelp();
         }
-      else if (keyword=="KroneckerProduct")
+      else if (keyword=="branching")
+        {
+          int lambda, mu;
+          
+          line_stream >> lambda >> mu; 
+          ParsingCheck(line_stream,line_count,line);
+
+          DoBranchingSO3(lambda, mu);
+        }
+      else if (keyword=="product")
         {
           int lambda1, mu1, lambda2, mu2;
           
@@ -167,10 +198,6 @@ int main(int argc, char **argv)
           ParsingCheck(line_stream,line_count,line);
 
           DoKroneckerProduct(lambda1, mu1, lambda2, mu2);
-        }
-      else if (keyword=="KroeneckerProduct")
-        {
-          std::cout << "No, that's not how it's spelled." << std::endl;
         }
       else if (keyword=="W")
         {
