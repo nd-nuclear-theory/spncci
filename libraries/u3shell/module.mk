4c280909a48eca020af68e51c2a08d487e33752c
Anna <mccoy.anna.e@gmail.com>
2021-02-12 18:32:37 -0800

    And the u3shell module.mk...

--- libraries/u3shell/module.mk
+++ libraries/u3shell/module.mk
@@ -7,11 +7,11 @@ $(eval $(begin-module))
 
 module_units_h := 
 module_units_cpp-h := tensor_labels relative_operator two_body_operator u3st_scheme
-module_units_cpp-h += u3spn_scheme two_body_branching relative_branching
+module_units_cpp-h += u3spn_scheme relative_branching
 module_units_cpp-h += upcoupling unu3 unit_tensor_expansion
 module_units_cpp-h += unit_tensor_space_u3s interaction_truncation
 # module_units_f := 
-module_programs_cpp += generate_unit_tensors_tb
+module_programs_cpp += 
 module_programs_cpp_test := tensor_labels_test relative_operator_test two_body_operator_test 
 module_programs_cpp_test += u3st_scheme_test u3spn_scheme_test upcoupling_test unu3_test
 module_programs_cpp_test += unit_tensor_space_u3s_test relative_branching_test interaction_truncation_test
