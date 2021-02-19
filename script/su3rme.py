17436626c4f3c2000c4736388fff665f54895db3
Anna <mccoy.anna.e@gmail.com>
2021-02-19 09:03:30 -0800

    updating su3rme scripting

--- script/su3rme.py
+++ script/su3rme.py
@@ -85,7 +85,6 @@
   2/16/18 (aem): Created with code extracted from spncci.py.
 
 """
-  
 import glob
 import mcscript
 import os
@@ -112,14 +111,8 @@ generate_lsu3shell_relative_operators_executable = os.path.join(project_root,"sp
 u3s_subspace_lister_executable=os.path.join(project_root,"spncci","programs","lgi","get_u3s_subspaces")
 
 ################################################################
-# relative operator construction (A-independent)
+# relative operator construction (input into LSU3Shell)
 ################################################################
-def setup_su3shell_directories(task):
-    su3rme_data_directory=os.environ["SU3SHELL_DATA"]
-    directory_name=su3rme_data_directory+"/rme"
-    mcscript.call(["mkdir","-p",directory_name])
-
-
 def relative_operator_descriptor(task):
     """Generate descriptor string for use in relative operator archive
     filename.
@@ -134,8 +127,24 @@ def relative_operator_descriptor(task):
 def generate_relative_operators(task):
 
     """
-    Create relative unit tensors and symplectic raising/lowering/N operators.
-    Invokes generate_lsu3shell_relative_operators.
+    Generates biquad representation of relative unit tensors, Arel, Brel and Nrel.
+
+    Invokes generate_lsu3shell_relative_operators in spncci/programs/unit_tensors/.
+
+    Output:
+        relative_operators.dat : Control file.  Each line of file is:
+                basename N lambda mu 2*S
+        
+            + basename is name of file containing operator with tensor character N(lambda,mu)S
+            + list of basenames includes all relative unit tensors, Arel, Brel and Nrel.
+        
+        relative_unit_00000i.recoupler : biquad representation of each unit tensor indexed by i.
+            Order of unit tensors determined by function u3shell::GenerateRelativeUnitTensorLabelsU3ST
+
+        Arel.recoupler : biquad representation of relative symplectic raising operator
+        Brel.recoupler : biquad representation of relative symplectic lowering operator
+        Nrel.recoupler : biquad representation of relative number operator
+
     """
 
     command_line = [
@@ -170,6 +179,47 @@ def read_relative_operator_basenames(task):
     relative_operator_stream.close()
     return relative_operator_basename_list
 
+
+def split_relative_operator_file(task):
+    ## split relative_operators.dat into two control files, one for generators 
+    ## and one for unit tensors.
+
+    # open up file relative_operators.dat and separate out relative_generators and 
+    # relative_operators 
+    relative_filename="relative_operators.dat"
+ 
+    unittensors=[];
+    generators=[];
+    file=open(relative_filename,'r')
+    for line in file:
+    # for line in lines:
+        operator=line.split()
+        if(operator[0]=="Arel" or operator[0]=="Brel" or operator[0]=="Nrel"):
+            generators.append(line)
+        else:
+            unittensors.append(line)
+
+    ## Write operator base names and labels to corresponding file 
+    generator_filename="relative_generator_operators.dat"
+    with open(generator_filename, 'w') as outstream:
+        for line in generators:
+            outstream.write(line)
+    outstream.close()
+
+    unittensor_filename="relative_unit_operators.dat"
+    with open(unittensor_filename, 'w') as outstream:
+        for line in unittensors:
+            outstream.write(line)
+    outstream.close()
+
+    # moving relative_operators.dat since name will be re-used as a simlink to
+    # relative_generators.dat or relative_unittensors.dat
+    mcscript.call(["mv","relative_operators.dat","relative_operators-old.dat"])
+
+
+
+
+
 def recouple_operators(task,relative_operator_basename_list):
     """ Invoke lsu3shell recoupler code on relative unit
     tensors and symplectic raising/lowering/N operators.
@@ -195,13 +245,15 @@ def recouple_operators(task,relative_operator_basename_list):
         )
 
 
-def save_operator_files(task):
+def save_operator_files_old(task):
     """ Create archive of relative operator files.
 
     Manual follow-up: The operator files are bundled into tgz files and saved to
     the current run's results directory.  They should then be moved
     (manually) to the directory specified in SPNCCI_LSU3SHELL_DIR, for
     subsequent use.
+
+    DEPRECATED
     """
 
     # select files to save
@@ -229,77 +281,133 @@ def save_operator_files(task):
             ]
         )
 
-def retrieve_operator_files(task):
-    """ Retrieve archive of relative operator files.
+
+def save_operator_files(task):
+    """ 
+    Create archive of relative operator files separated into two archives
+        relative-unit-operators-{descriptor}.tgz :  Contains all relative unit tensor files
+        relative-generator-operators-{descrptor}.tgz : Contains symplectic generators (Arel,Brel,Nrel)
+    
+    Results are saved to current run's results directory and need to be (manually) moved to
+    directory specified in SPNCCI_LSU3SHELL_DIR for subsequent use.
     """
 
-    # identify archive file
+    ## Split up relative_operators.dat into two files, one for unit tensors, one for generators 
+    split_relative_operator_file(task)
+
+    ## Define descriptor for archive file
     descriptor = relative_operator_descriptor(task)
-    filename="relative-operators-{}.tgz".format(descriptor)
-    print(filename)
+    
+    # select unit tensor files to save
+    unit_tensor_archive_list = glob.glob("relative_unit_operators.dat")
+    unit_tensor_archive_list += glob.glob("relative_unit*.PN")
+    unit_tensor_archive_list += glob.glob("relative_unit*.PPNN")
+    
+    unit_tensor_archive_filename = f"relative-unit-operators-{descriptor}.tgz"
+    mcscript.call(
+        ["tar", "-zcvf", unit_tensor_archive_filename] + unit_tensor_archive_list
+    )
+
+    # Select generator files to save
+    generators_archive_list = glob.glob("relative_generator_operators.dat")
+    generators_archive_list += glob.glob("*rel.PN")
+    generators_archive_list += glob.glob("*rel.PPNN")
+    
+    generators_archive_filename = f"relative-generator-operators-{descriptor}.tgz"
+    mcscript.call(
+        ["tar", "-zcvf", generators_archive_filename] + generators_archive_list
+    )
+
+    # move archive to results directory (if in multi-task run)
+    if (mcscript.task.results_dir is not None):
+        mcscript.call(
+            [
+                "mv",
+                "--verbose",
+                unit_tensor_archive_filename,
+                "--target-directory={}".format(mcscript.task.results_dir)
+            ]
+        )
+
+    if (mcscript.task.results_dir is not None):
+        mcscript.call(
+            [
+                "mv",
+                "--verbose",
+                generators_archive_filename,
+                "--target-directory={}".format(mcscript.task.results_dir)
+            ]
+        )
+
+
+def retrieve_archived_files(filename):
+    """
+    Retrieve archived files and untar. 
+    """
+    
     archive_filename = mcscript.utils.search_in_subdirectories(
         operator_directory_list,
         operator_subdirectory_list,
         filename,
-        error_message="relative operator archive file {} not found".format(filename)
+        error_message=f"relative unit operator archive file {filename} not found"
     )
-
+    print(archive_filename)
     # extract archive contents
-    mcscript.call(
-        [
-            "tar", "xf", archive_filename
-        ]
-    )
-def split_relative_operator_file(task):
-    ## split relative_operators.dat into two control files, one for generators 
-    ## and one for unit tensors.
+    mcscript.call(["tar", "xf", archive_filename])
 
-    # open up file relative_operators.dat and separate out relative_generators and 
-    # relative_operators 
-    relative_filename="relative_operators.dat"
- 
-    unittensors=[];
-    generators=[];
-    file=open(relative_filename,'r')
-    for line in file:
-    # for line in lines:
-        operator=line.split()
-        if(operator[0]=="Arel" or operator[0]=="Brel" or operator[0]=="Nrel"):
-            generators.append(line)
-        else:
-            unittensors.append(line)
 
-    ## Write operator base names and labels to corresponding file 
-    generator_filename="relative_generators.dat"
-    with open(generator_filename, 'w') as outstream:
-        for line in generators:
-            outstream.write(line)
-    outstream.close()
+def retrieve_unit_operator_files(task):
+    """
+    Retrieve relative unit tensor operator files.
+    """
+    descriptor = relative_operator_descriptor(task)
+    filename=f"relative-unit-operators-{descriptor}.tgz"
+    retrieve_archived_files(filename)
 
-    unittensor_filename="relative_unittensors.dat"
-    with open(unittensor_filename, 'w') as outstream:
-        for line in unittensors:
-            outstream.write(line)
-    outstream.close()
 
-    # moving relative_operators.dat since name will be re-used as a simlink to
-    # relative_generators.dat or relative_unittensors.dat
-    mcscript.call(["mv","relative_operators.dat","relative_operators-old.dat"])
+def retrieve_generator_operator_files(task):
+    """
+    Retrieve relative generator operator files.
+    """
+    descriptor = relative_operator_descriptor(task)
+    filename=f"relative-generator-operators-{descriptor}.tgz"
+    retrieve_archived_files(filename)
+
+
+
+def retrieve_operator_files(task):
+    """ 
+    Retrieve relative unit tensor and generator operator files.
+    
+    """
+    retrieve_unit_operator_files(task)
+    retrieve_generator_operator_files(task)
+
+
+def convert_old_operator_archive(task):
+    """
+    Convert old archived operators (save_operator_files_old) to new 
+    archive structure (save_operator_files)
+
+    input: 
+
+        task (dictionary) : dictionary containing 
+            "N1v", "Nsigma_max", "Nstep"
+        
+    """
+    ## Revieve archive with old structure
+    descriptor = relative_operator_descriptor(task)
+    old_archive_filename = "relative-operators-{}.tgz".format(descriptor)
+    retrieve_archived_files(old_archive_filename)
+
+    ## Generate new archives for new structure 
+    save_operator_files(task)
+
 
 
 ################################################################
-# relative operator SU(3) RME construction
+# LSU3Shell basis setup
 ################################################################
-
-# su3rme descriptor string
-#
-# This describes the many-body space on which SU(3) RMEs are being
-# calculated.  It is used, e.g., in the filename of the archive file
-# in which the SU(3) RMEs are stored.
-
-# descriptor string for straightforward case of pure Nsigmamax truncation
-su3rme_descriptor_template_Nsigmamax = "Z{nuclide[0]:02d}-N{nuclide[1]:02d}-Nsigmamax{Nsigma_max:02d}-Nstep{Nstep:d}"
-
 def generate_model_space_file(task):
     """
     Create LSU3shell model space files for SU3RME:
@@ -311,92 +419,30 @@ def generate_model_space_file(task):
 
     Otherwise, copy truncated model space to model space input files. model_space_bra.dat and 
     """
+    model_space_executable_command_line=[
+        generate_lsu3shell_model_space_executable,
+        "{nuclide[0]:d}".format(**task),
+        "{nuclide[1]:d}".format(**task),
+        "{Nsigma_max:d}".format(**task),
+        "{Nstep:d}".format(**task)
+    ]
+
     if task["model_space_file_bra"]==None:
         print("generating model_space.dat")
-        command_line = [
-            generate_lsu3shell_model_space_executable,
-            "{nuclide[0]:d}".format(**task),
-            "{nuclide[1]:d}".format(**task),
-            "{Nsigma_max:d}".format(**task),
-            "{Nstep:d}".format(**task)
-        ]
-        mcscript.call(
-            command_line,
-            mode=mcscript.CallMode.kSerial
-        )
+        
+        mcscript.call(command_line,mode=mcscript.CallMode.kSerial)
         mcscript.call(["cp","model_space.dat","model_space_bra.dat"])
 
     else:
         mcscript.call(["cp",task["model_space_file_bra"], "model_space_bra.dat"])
 
-
     if task["model_space_file_ket"]==None:
-        command_line = [
-            generate_lsu3shell_model_space_executable,
-            "{nuclide[0]:d}".format(**task),
-            "{nuclide[1]:d}".format(**task),
-            "{Nsigma_max:d}".format(**task),
-            "{Nstep:d}".format(**task)
-        ]
-        mcscript.call(
-            command_line,
-            mode=mcscript.CallMode.kSerial
-        )
-
+        mcscript.call(command_line,mode=mcscript.CallMode.kSerial)
         mcscript.call(["cp","model_space.dat","model_space_ket.dat"])
 
     else:
         mcscript.call(["cp",task["model_space_file_ket"], "model_space_ket.dat"])
 
-
-def generate_u3s_subspace_labels(task):
-    # TODO 10/07/20 (mac): update to new simple filename arguments for
-    # get_u3s_subspaces (see example handler in lsu3shell.py)
-    raise mcscript.ScriptError("Need to update generate_u3s_subspace_labels")
-    (Z,N)=task["nuclide"]
-    mcscript.call([
-        u3s_subspace_lister_executable,
-        "{nuclide[0]:d}".format(**task),
-        "{nuclide[1]:d}".format(**task),
-        "lsu3shell_basis.dat",
-        "{Nsigma_max:d}".format(**task)
-        ])
-
-
-# def do_get_u3s_labels(task):
-#   # generate model space file needed by lsu3shell codes
-#   su3rme.generate_model_space_file(task)
-#   # generate basis listing for basis in which rmes are calculated
-#   su3rme.generate_basis_table(task)
-#   su3rme.generate_u3s_subspace_labels(task)
-
-
-
-def calculate_rmes(task):
-    """ Invoke lsu3shell SU3RME code to calculate rmes of relative unit
-    tensors and symplectic raising/lowering/N operators in SU(3)-NCSM basis.
-
-    Invokes SU3RME_MPI.
-    """
-
-    model_space_filename_bra = "model_space_bra.dat"
-    model_space_filename_ket = "model_space_ket.dat"
-
-    if ("su3rme_mode" not in task):
-        task["su3rme_mode"] = "text"
-
-    # call SU3RME
-    command_line = [
-        su3rme_executable,
-        model_space_filename_bra,
-        model_space_filename_ket,
-        "relative_operators.dat",
-        task["su3rme_mode"]
-    ]
-    mcscript.call(
-        command_line,
-        mode=mcscript.CallMode.kHybrid
-    )
 
 
 def generate_basis_table(task):
@@ -416,6 +462,66 @@ def generate_basis_table(task):
         command_line,
         mode=mcscript.CallMode.kSerial
     )
+
+
+def generate_u3s_subspace_labels(task):
+    # TODO 10/07/20 (mac): update to new simple filename arguments for
+    # get_u3s_subspaces (see example handler in lsu3shell.py)
+    #
+    # (aem) : @Mark, where is lsu3shell.py? 
+    
+    raise mcscript.ScriptError("Need to update generate_u3s_subspace_labels")
+    (Z,N)=task["nuclide"]
+    mcscript.call([
+        u3s_subspace_lister_executable,
+        "{nuclide[0]:d}".format(**task),
+        "{nuclide[1]:d}".format(**task),
+        "lsu3shell_basis.dat",
+        "{Nsigma_max:d}".format(**task)
+        ])
+
+
+################################################################
+# relative operator SU(3) RME construction
+################################################################
+# su3rme descriptor string: describes the many-body space on which SU(3) 
+#   RMEs are calculated.  It is used, e.g., in the filename of the archive file
+#   in which the SU(3) RMEs are stored.
+# descriptor string for straightforward case of pure Nsigmamax truncation
+su3rme_descriptor_template_Nsigmamax = "Z{nuclide[0]:02d}-N{nuclide[1]:02d}-Nsigmamax{Nsigma_max:02d}-Nstep{Nstep:d}"
+
+
+def setup_su3shell_directories(task):
+    """
+    Make sure temporary work directory exists
+    """
+    su3rme_data_directory=os.environ["SU3SHELL_DATA"]
+    directory_name=su3rme_data_directory+"/rme"
+    mcscript.call(["mkdir","-p",directory_name])
+
+
+def calculate_rmes(task):
+    """ Invoke lsu3shell SU3RME code to calculate rmes of relative unit
+    tensors and symplectic raising/lowering/N operators in SU(3)-NCSM basis.
+
+    Invokes SU3RME_MPI.
+    """
+    if ("su3rme_mode" not in task):
+        task["su3rme_mode"] = "text"
+
+    # call SU3RME
+    command_line = [
+        su3rme_executable,
+        "model_space_bra.dat",
+        "model_space_ket.dat",
+        "relative_operators.dat",
+        task["su3rme_mode"]
+    ]
+    mcscript.call(
+        command_line,
+        mode=mcscript.CallMode.kHybrid
+    )
+
 
 def save_su3rme_files(task):
     """Create archive of SU(3) RMEs of relative operators.
@@ -543,7 +649,7 @@ def generate_lsu3shell_rmes(task):
     """
     Wrapper for relative_operator.dat independent part of main function
     """
-    # print(task)
+
     # set up necessary directories for lsu3shell
     setup_su3shell_directories(task)
 
