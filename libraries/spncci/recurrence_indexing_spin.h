f05dee2b2587716b05e9cb193ac6479df3aa6b45
Patrick Fasano <patrick@patrickfasano.com>
2021-09-29 01:45:50 -0400

    recurrence_indexing_spin: Replace Sp/Sn constraints with constraints from brute-force enumeration

--- libraries/spncci/recurrence_indexing_spin.h
+++ libraries/spncci/recurrence_indexing_spin.h
@@ -695,13 +695,44 @@ inline bool UnitTensorAllowed(
     UnitTensorLabelsST tensor_labels,
     const lgi::LGI::UpstreamLabelsType& bra_labels
   )
+// Check if bra and ket labels can be connected by a given ST tensor.
+//
+// Note: these checks were derived by brute force enumeration of the possible
+// $\bar{S}_p' \bar{S}_n' \bar{S}_p \bar{S}_n$ for states with given S, T and Tz,
+// followed by checking all possible products and selecting those which satisfy
+// all the relevant triangle constraints for coupling to good $S_{p,0} \times
+// S_{n,0} \rightarrow S_0$.
 {
-  if ((tensor_labels.S0() == 0) && (tensor_labels.T0() == 0))
+  const HalfInt& bra_Sp=bra_labels.Sp, bra_Sn=bra_labels.Sn;
+  const int S0=tensor_labels.S0();
+  const HalfInt& ket_Sp=ket_labels.Sp, ket_Sn=ket_labels.Sn;
+
+  if (am::AllowedTriangle(bra_Sp,1,ket_Sp) && am::AllowedTriangle(bra_Sn,1,ket_Sn))
+    return true;
+
+  if (S0==0)
   {
-    if ((ket_labels.Sp != bra_labels.Sp) || (ket_labels.Sn != bra_labels.Sn))
-      return false;
+    if (bra_Sp==ket_Sp && bra_Sn==ket_Sn)
+      return true;
   }
-  return true;
+  else if (S0==1)
+  {
+    if (bra_Sp==ket_Sp && am::AllowedTriangle(bra_Sn,1,ket_Sn))
+      return true;
+
+    if (am::AllowedTriangle(bra_Sp,1,ket_Sp) && bra_Sn==ket_Sn)
+      return true;
+  }
+  else if (S0==2 && tensor_labels.Tbarp()==1 && tensor_labels.Tbar()==1)
+  {
+    if (bra_Sp==ket_Sp && am::AllowedTriangle(bra_Sn,2,ket_Sn))
+      return true;
+
+    if (am::AllowedTriangle(bra_Sp,2,ket_Sp) && bra_Sn==ket_Sn)
+      return true;
+  }
+
+  return false;
 }
 
 }  // namespace spncci::spin
