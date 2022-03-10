0811bb4d144230ec19e1d469c158adf3721512dd
Anna <amccoy@nd.edu>
2022-03-09 23:59:19 -0800

    Update get_u3s_subspaces function for generating input files for Lanczos decompositions to directly generate lsu3shell basis dimensions.

--- programs/lgi/get_u3s_subspaces.cpp
+++ programs/lgi/get_u3s_subspaces.cpp
@@ -3,25 +3,25 @@
 
   Syntax:
 
-    get_u3s_subspaces basis_filename dimensions_filename
-
-      basis_filename: input LSU3shell basis filename, typically
-      Z{}-N{}-Nmax{:02d}_basis.dat
+    get_u3s_subspaces Z N Nmax dimensions_filename
 
       dimensions_filename: output U3S dimensions filename, typically
       u3s_subspace_labels_Z{}_N{}_Nmax{:02d}.dat
 
   Example:
  
-    get_u3s_subspaces ~/results/mcaprio/lsu3shell/runmac0549/results/Z3-N3-Nmax06_basis.dat u3s_subspace_labels_Z3_N3_Nmax06.dat
+    get_u3s_subspaces 3 3 6 u3s_subspace_labels_Z3_N3_Nmax06.dat
 
-  Anna E. McCoy and Mark A. Caprio
-  University of Notre Dame and TRIUMF
+  Anna E. McCoy[2,3] and Mark A. Caprio[1]
+  [1] University of Notre Dame
+  [2] TRIUMF
+  [3] Institute for Nuclear Theory
 
   SPDX-License-Identifier: MIT
 
   11/12/19 (aem): Created.
   08/29/20 (mac): Simplify command line arguments.
+  03/09/22 (aem): Generate lsu3shell basis rather than read from file.
 
 ****************************************************************/
 #include <fstream>
@@ -29,102 +29,59 @@
 #include <sstream>
 #include <map>
 
-// #include "spncci/computation_control.h"
 #include "fmt/format.h"
 #include "am/halfint.h"
 #include "sp3rlib/u3.h"
 #include "sp3rlib/u3coef.h"
-// #include "u3shell/u3spn_scheme.h"
-// #include "spncci/results_output.h"
+#include "utilities/nuclide.h"
+#include "u3ncsm/dimensions.h"
 
 namespace lsu3shell
 {
-  void ReadLSU3ShellBasisAndAccumulate(
-      const std::string& filename, 
-      std::map<std::tuple<int,int,int,int,int,int>,int>& u3s_subspace_labels_set
-    )
-  // Read basis states labels from LSU3Shell basis file and accumulate by N(lambda,mu)S labels
-  // Based on ReadLSU3ShellBasis in lsu3shell/lsu3shell_basis.cpp
-  {
-    // open basis file
-    std::ifstream basis_stream(filename.c_str());
-    if(not basis_stream)
-      std::cout<<fmt::format("File {} did not open",filename)<<std::endl;
 
-    // read basis table entries
-    std::string line;
-    while(std::getline(basis_stream,line))
-      {
-        // skip initial header line
-        if(not std::isdigit(line[0]))
-          continue;
-
-        // parse line
-        std::istringstream line_stream(line);
-        // alpha_n_max and alpha_p_max correspond multiplicities arising in the coupling of protons among shells and 
-        // neutrons among shells. rho0_max is outer multplicity of coupling proton to neutron. 
-        int Np, Nn, Nex,twice_Sp, twice_Sn, twice_S, lambda, mu, ip,in,alpha_p_max,alpha_n_max,rho0_max,lambda_p,mu_p,lambda_n,mu_n;
-        line_stream
-          >>ip>>alpha_p_max>>Np>>lambda_p>>mu_p>>twice_Sp 
-          >>in>>alpha_n_max>>Nn>>lambda_n>>mu_n>>twice_Sn 
-          >>rho0_max>>lambda >> mu>>twice_S;
-
-        // convert parameter data types
-        Nex=Nn+Np;
-        HalfInt Sp=HalfInt(twice_Sp,2);
-        HalfInt Sn=HalfInt(twice_Sn,2);
-        HalfInt S=HalfInt(twice_S,2);
-        u3::SU3 xp(lambda_p,mu_p);
-        u3::SU3 xn(lambda_n,mu_n);
-        u3::SU3 x(lambda,mu);
-
-        // perform sanity checks on input basis labels
-        assert(am::AllowedTriangle(Sp,Sn,S));
-        assert(u3::OuterMultiplicity(xp,xn,x)==rho0_max);
-
-        int dim=alpha_n_max*alpha_p_max*rho0_max;
-        std::tuple<int,int,int,int,int,int> labels(Nex,lambda,mu,twice_Sp,twice_Sn,twice_S);
-        u3s_subspace_labels_set[labels]+=dim;
-      }
-
-    // done with basis file
-    basis_stream.close();
-  }
-
-void WriteU3SLabels(std::string filename, const std::map<std::tuple<int,int,int,int,int,int>,int>& u3s_subspace_labels_set)
-// Write U3S labels and dimensions.
+void WriteU3SLabels(
+  const HalfInt& Nsigma0,
+  std::string filename,
+  const std::map<u3shell::U3SPN,unsigned int>& u3s_subspace_labels_set
+  )
+// Write U3SpSnS labels and dimensions.
   {
     std::ofstream outfile;
     outfile.open(filename);
-    for(auto it=u3s_subspace_labels_set.begin(); it!=u3s_subspace_labels_set.end(); ++it)
+    for(const auto& [subspace_labels,dim] : u3s_subspace_labels_set)
       {
-        int Nex,lambda,mu,twice_S,twice_Sp,twice_Sn;
-        std::tie(Nex,lambda,mu,twice_Sp,twice_Sn,twice_S)=it->first;
-        int dim=it->second;
-        outfile<<fmt::format("{:2d} {:3d} {:3d} {:3d} {:3d} {:3d} {:4d}",Nex,lambda,mu,twice_Sp,twice_Sn,twice_S,dim)<<std::endl;
+        const auto& [omega,Sp,Sn,S] = subspace_labels.FlatKey();
+        const auto& x = omega.SU3();
+        outfile<<fmt::format("{:2d} {:3d} {:3d} {:3d} {:3d} {:3d} {:4d}",
+          int(omega.N()-Nsigma0),x.lambda(),x.mu(),TwiceValue(Sp),TwiceValue(Sn),TwiceValue(S),dim
+        )<<std::endl;
       }
     outfile.close();
   }
 
-void WriteU3SBranchedLabels(std::string filename, const std::map<std::tuple<int,int,int,int,int,int>,int>& u3s_subspace_labels_set)
+void WriteU3SBranchedLabels(
+  const HalfInt& Nsigma0,
+  std::string filename,
+  const std::map<u3shell::U3SPN,unsigned int>& u3s_subspace_labels_set
+  )
+  // Write U3LSpSnS labels and dimensions
   {
+
     std::ofstream outfile;
     outfile.open(filename);
-    for(auto it=u3s_subspace_labels_set.begin(); it!=u3s_subspace_labels_set.end(); ++it)
+    for(const auto& [subspace_labels,dim] : u3s_subspace_labels_set)
       {
-        int Nex,lambda,mu,twice_Sp,twice_Sn,twice_S;
-        std::tie(Nex,lambda,mu,twice_Sp,twice_Sn,twice_S)=it->first;
-        int dim=it->second;
-        MultiplicityTagged<unsigned int>::vector branched_states=u3::BranchingSO3(u3::SU3(lambda,mu));
-        for(auto tagged_L : branched_states)
+        const auto& [omega,Sp,Sn,S] = subspace_labels.FlatKey();
+        const auto& x = omega.SU3();
+        int Nex = int(omega.N()-Nsigma0);
+
+        MultiplicityTagged<unsigned int>::vector branched_states=u3::BranchingSO3(x);
+        for(const auto& [L,kappa_max] : branched_states)
           {
-            int L=tagged_L.irrep;
-            int kappa_max=tagged_L.tag;
             for(int kappa=1; kappa<=kappa_max; ++kappa)
               outfile<<fmt::format("{:2d} {:3d} {:3d} {:3d} {:3d} {:4d}",
-                Nex,lambda,mu,kappa,L,twice_Sp,twice_Sn,twice_S,dim)<<std::endl;    
+                Nex,x.lambda(),x.mu(),kappa,L,TwiceValue(Sp),TwiceValue(Sn),TwiceValue(S),dim)<<std::endl;
           }
-        
       }
     outfile.close();
   }
@@ -134,24 +91,29 @@ void WriteU3SBranchedLabels(std::string filename, const std::map<std::tuple<int,
 int main(int argc, char **argv)
 {
   
-  if(argc<2+1)
+  if(argc<4+1)
   {
-    // std::cout<<"Syntax: nucleus_label filename"<<std::endl;
+    std::cout<<"Syntax: Z N Nmax outputfile_name"<<std::endl;
     exit(0);
   }
 
-  std::string lsu3shell_filename=argv[1];
-  std::string u3s_filename=argv[2];
+  int Z = std::stoi(argv[1]);
+  int N = std::stoi(argv[2]);
+  int Nmax = std::stoi(argv[3]);
+  nuclide::NuclideType nuclide({Z,N});
+  std::string u3s_filename=argv[4];
 
-    // SU(3) caching
-  int max_lambda_plus_mu=39;
-  u3::U3CoefInit(max_lambda_plus_mu);
+  // // SU(3) caching
+  // int max_lambda_plus_mu=39;
+  // u3::U3CoefInit(max_lambda_plus_mu);
 
-  
-  std::map<std::tuple<int,int,int,int,int,int>,int> u3s_subspace_labels_set;
-  lsu3shell::ReadLSU3ShellBasisAndAccumulate(lsu3shell_filename, u3s_subspace_labels_set);
-  
-  lsu3shell::WriteU3SLabels(u3s_filename,u3s_subspace_labels_set);
+  HalfInt Nsigma0 = nuclide::Nsigma0ForNuclide(nuclide);
+
+  auto u3s_subspace_labels_set
+    = lsu3shell::lsu3shell_basis_dimensions(nuclide,Nsigma0,Nmax);
+
+
+  lsu3shell::WriteU3SLabels(Nsigma0,u3s_filename,u3s_subspace_labels_set);
   
   // std::string u3sl_filename=fmt::format("u3sl_subspace_labels_Z{}_N{}_Nmax{:02d}.dat",Z,N,Nmax);
   // lsu3shell::WriteU3SBranchedLabels(u3sl_filename,u3s_subspace_labels_set);
