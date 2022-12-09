91e40423c137b0bcf458e3d21cf063966079e54e
Anna McCoy <amccoy@alumni.nd.edu>
2022-12-09 12:54:11 -0800

    libraries/u3ncsm: updating namespaces.

--- libraries/u3ncsm/u3ncsm_interface.cpp
+++ libraries/u3ncsm/u3ncsm_interface.cpp
@@ -7,53 +7,52 @@
   SPDX-License-Identifier: MIT
 ****************************************************************/
 #include "u3ncsm/u3ncsm_interface.h"
-
 #include "mcutils/eigen.h"
 #include "mcutils/parsing.h"
 #include "utilities/utilities.h"
 
 
-namespace spncci::seeds{
+namespace lgi
+{
 
-    std::unordered_map<u3shell::U3SPN,basis::OperatorBlock<double>>
-    GetLGIExpansions(
-      const nuclide::NuclideType& nuclide,
-      const spncci::spin::RecurrenceLGISpace<lgi::LGI,u3shell::spin::twobody::OperatorLabelsST>& lgi_space,
-      const HalfInt& Nsigma0
-      )
+std::unordered_map<u3shell::U3SPN, basis::OperatorBlock<double>>
+GetLGIExpansions(
+    const nuclide::NuclideType& nuclide,
+    const spncci::spin::RecurrenceLGISpace<lgi::LGI, u3shell::spin::twobody::OperatorLabelsST>&
+        lgi_space,
+    const HalfInt& Nsigma0
+  )
+{
+  const auto& [Z, N] = nuclide;
+  std::unordered_map<u3shell::U3SPN, basis::OperatorBlock<double>> lgi_expansions;
+  std::unordered_set<lgi::LGI> temp_list;
+  const auto& [sigma_ket, sigma_bra, exchange_symm_bar] = lgi_space.labels();
+  int Nex_bra = int(sigma_bra.N() - Nsigma0);
+  int Nex_ket = int(sigma_ket.N() - Nsigma0);
+  for (const auto& spin_space : lgi_space)
+  {
+    const auto& [S_ket, S_bra] = spin_space.labels();
+    for (const auto& spin_subspace : spin_space)
     {
-      const auto&[Z,N] = nuclide;
-      std::unordered_map<u3shell::U3SPN,basis::OperatorBlock<double>> lgi_expansions;
-      std::unordered_set<lgi::LGI> temp_list;
-      const auto& [sigma_ket,sigma_bra,exchange_symm_bar] = lgi_space.labels();
-      int Nex_bra = int(sigma_bra.N()-Nsigma0);
-      int Nex_ket = int(sigma_ket.N()-Nsigma0);
-      for(const auto& spin_space : lgi_space)
-        {
-          const auto& [S_ket, S_bra] = spin_space.labels();
-          for(const auto& spin_subspace : spin_space)
-            {
-              const auto& ket_upstream_labels = spin_subspace.ket_upstream_labels();
-              const auto& bra_upstream_labels = spin_subspace.bra_upstream_labels();
-              const auto [Sp_ket, Sn_ket] = ket_upstream_labels;
-              const auto [Sp_bra, Sn_bra] = bra_upstream_labels;
+      const auto& ket_upstream_labels = spin_subspace.ket_upstream_labels();
+      const auto& bra_upstream_labels = spin_subspace.bra_upstream_labels();
+      const auto [Sp_ket, Sn_ket] = ket_upstream_labels;
+      const auto [Sp_bra, Sn_bra] = bra_upstream_labels;
 
-              temp_list.insert({{sigma_bra,Sp_bra,Sn_bra,S_bra},Nex_bra});
-              temp_list.insert({{sigma_ket,Sp_ket,Sn_ket,S_ket},Nex_ket});
-            }
-        }
-
-      for(const auto& lgi : temp_list)
-        {
-          std::string filename = lgi::lgi_expansion_filename(Z,N,lgi);
-
-          // TODO: Make construction optional if lgi expansion doesn't exist
-          bool exit_on_nonexist=true, warn_on_overwrite=false;
-          mcutils::FileExistCheck(filename,exit_on_nonexist,warn_on_overwrite);
-          lgi_expansions[lgi.u3spn()] = utils::ReadOperatorBlockBinary(filename);
-        }
-
-      return lgi_expansions;
+      temp_list.insert({{sigma_bra, Sp_bra, Sn_bra, S_bra}, Nex_bra});
+      temp_list.insert({{sigma_ket, Sp_ket, Sn_ket, S_ket}, Nex_ket});
     }
+  }
 
-}// namespaces
+  for (const auto& lgi : temp_list)
+  {
+    std::string filename = lgi::lgi_expansion_filename(Z, N, lgi);
+    bool exit_on_nonexist = true, warn_on_overwrite = false;
+    mcutils::FileExistCheck(filename, exit_on_nonexist, warn_on_overwrite);
+    lgi_expansions[lgi.u3spn()] = utils::ReadOperatorBlockBinary(filename);
+  }
+
+  return lgi_expansions;
+}
+
+}  // namespace lgi
--- libraries/u3ncsm/u3ncsm_interface.h
+++ libraries/u3ncsm/u3ncsm_interface.h
@@ -15,58 +15,80 @@
 #define U3NCSM_INTERFACE_H_
 
 #include <unordered_map>
+
 #include "am/halfint.h"
 #include "fmt/format.h"
-
 #include "lgi/lgi.h"
 // #include "lgi/recurrence_lgi.h"
 #include "spncci_basis/recurrence_indexing.h"
 #include "u3shell/relative_operator.h"
 #include "utilities/nuclide.h"
 
-namespace spncci::seeds{
+namespace spncci::seeds
+{
 
-  inline std::string seed_filename(
-    const int Z, const int N,
+inline std::string seed_filename(
+    const int Z,
+    const int N,
     const HalfInt& Nsigma0,
     const u3::U3& sigma_bra,
     const u3::U3& sigma_ket,
     const int parity_bar
-    )
-    {
-      int Nex_bra(sigma_bra.N()-Nsigma0);
-      int Nex_ket(sigma_ket.N()-Nsigma0);
+  )
+{
+  int Nex_bra(sigma_bra.N() - Nsigma0);
+  int Nex_ket(sigma_ket.N() - Nsigma0);
 
-      std::string filename
-        = fmt::format("seeds_Z{:02d}_N{:02d}_Nex{:02d}_lm{:02d}_mu{:02d}_Nex{:02d}_lm{:02d}_mu{:02d}_gbar{:1d}.dat",
-              Z,N,Nex_bra,sigma_bra.SU3().lambda(),sigma_bra.SU3().mu(),
-              Nex_ket,sigma_ket.SU3().lambda(),sigma_ket.SU3().mu(),parity_bar
-            );
+  std::string filename = fmt::format(
+      "seeds_Z{:02d}_N{:02d}_Nex{:02d}_lm{:02d}_mu{:02d}_Nex{:02d}_lm{:02d}_mu{"
+      ":02d}_gbar{:1d}.dat",
+      Z,
+      N,
+      Nex_bra,
+      sigma_bra.SU3().lambda(),
+      sigma_bra.SU3().mu(),
+      Nex_ket,
+      sigma_ket.SU3().lambda(),
+      sigma_ket.SU3().mu(),
+      parity_bar
+    );
 
-      return filename;
-    }
-
-    std::unordered_map<u3shell::U3SPN,basis::OperatorBlock<double>>
-    GetLGIExpansions(
-      const nuclide::NuclideType& nuclide,
-      const spncci::spin::RecurrenceLGISpace<lgi::LGI,u3shell::spin::twobody::OperatorLabelsST>& lgi_space,
-      const HalfInt& Nsigma0
-      );
-
-
+  return filename;
 }
 
-namespace lgi{
 
-  inline std::string lgi_expansion_filename(const int Z, const int N, const lgi::LGI& lgi)
-    {
-      const auto&[Nex,sigma,Sp,Sn,S]=lgi.Key();
-      std::string filename
-        = fmt::format("lgi_expansion_Z{:02d}_N{:02d}_Nex{:02d}_lm{:02d}_mu{:02d}_2Sp{:02}_2Sn{:02}_2S{:02}.dat",
-              Z,N,Nex,sigma.SU3().lambda(),sigma.SU3().mu(),TwiceValue(Sp),TwiceValue(Sn),TwiceValue(S));
-      return filename;
-    }
-  }
+}  // namespace spncci::seeds
+
+namespace lgi
+{
+
+inline std::string lgi_expansion_filename(const int Z, const int N, const lgi::LGI& lgi)
+{
+  const auto& [Nex, sigma, Sp, Sn, S] = lgi.Key();
+  std::string filename = fmt::format(
+      "lgi_expansion_Z{:02d}_N{:02d}_Nex{:02d}_lm{:02d}_mu{:02d}_2Sp{:02}_2Sn{:"
+      "02}_2S{:02}.dat",
+      Z,
+      N,
+      Nex,
+      sigma.SU3().lambda(),
+      sigma.SU3().mu(),
+      TwiceValue(Sp),
+      TwiceValue(Sn),
+      TwiceValue(S)
+    );
+  return filename;
+}
+
+std::unordered_map<u3shell::U3SPN, basis::OperatorBlock<double>> GetLGIExpansions(
+    const nuclide::NuclideType& nuclide,
+    const spncci::spin::RecurrenceLGISpace<lgi::LGI, u3shell::spin::twobody::OperatorLabelsST>&
+        lgi_space,
+    const HalfInt& Nsigma0
+  );
+/// Reads in LGI expansions from file.
+
+}  // namespace lgi
 
 
 #endif
